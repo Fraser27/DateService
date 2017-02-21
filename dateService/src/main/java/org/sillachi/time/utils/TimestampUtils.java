cbb56c2e2024b47fb25862e05d3d392be628e320
Sillachi <leo27fraser@gmail.com>
2017-02-22 02:40:42 +0530

    TimeStamp changes using Java 8 Time Api

--- dateService/src/main/java/org/sillachi/time/utils/TimestampUtils.java
+++ dateService/src/main/java/org/sillachi/time/utils/TimestampUtils.java
@@ -2,9 +2,19 @@ package org.sillachi.time.utils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Set;
 import java.util.TimeZone;
+
+import org.sillachi.model.TimeStampModel;
+import org.springframework.beans.propertyeditors.ZoneIdEditor;
 
 
 /**
@@ -16,27 +26,18 @@ public class TimestampUtils {
 
 	/**
 	 * Return an ISO 8601 combined date and time string for current date/time
-	 * 
+	 * In IST format
 	 * @return String with format "yyyy-MM-dd'T'HH:mm:ss'Z'"
 	 */
 	public static String getISO8601StringForCurrentDate() {
-		Date now = new Date();
-		return getISO8601StringForDate(now);
+		Instant now = Instant.now();
+		return now.toString().substring(0,now.toString().length()-1)+"+05:30[Asia/Calcutta]";
 	}
 
-	/**
-	 * Return an ISO 8601 combined date and time string for specified date/time
-	 * 
-	 * @param date
-	 *            Date
-	 * @return String with format "yyyy-MM-dd'T'HH:mm:ss'Z'"
-	 */
-	private static String getISO8601StringForDate(Date date) {
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
-		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-		return dateFormat.format(date);
-	}
-	
 	private TimestampUtils() {
 	}
+	
+   public static void main(String[] args) {
+	System.out.println(TimestampUtils.getISO8601StringForCurrentDate());
+}
 }
